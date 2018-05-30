4eb09992d3d363a46653a1716b84777c6bd55b5c
Roy Chapman <rdc97@uw.edu>
2018-05-30 11:58:06 -0700

    ayyye text conclusions, hypoth etc

--- our_ui.R
+++ our_ui.R
@@ -21,7 +21,27 @@ ui <- fluidPage(
                  selectInput("x_var", label = "X Variable", choices = list("In State Tuition", "Out of State Tuition", "Loan Ever"))
                ),
                mainPanel(
-                 plotlyOutput("roy_plot")
+                 plotlyOutput("roy_plot"),
+                 strong(h1("Median Graduation Debt Data")),
+                  p("The plot above showes the relationship betweenthe Median Graduation Debt and the resepective X variable choosen. In this section we are concerned about the relationship
+                   the two varibales and to answer three questions."),
+                 em(p("What is the relationship between In State Tuition and the Median Debt Graduation Debt?")),
+                 em(p("What is the relationship between the Out of State Tuiition and Median Debt Graduation Debt and how does that compare to In State?")),
+                 em(p("Of these two characteristics, what is the relationship between people who have taken out a Loan Ever and the Median Graduation Debt")),
+                 strong(h2("Hypothesis")),
+                 p("1. The Median Graduation Debt would be high among those who are non-residents and pay higher tuitions."),
+                 p("2. Schools that average higher levels of students that take out loans will have a higher median of Graduation Debt"),
+                 strong(h2("Conclusions")),
+                 p("1. For both In State and Out of State Tuition rates the median debt has very little influence. We found this as a surprise as we assumed a 
+                   more expensive college would yield a higher debt median, instead, the points just moved right (increase tuition) rather than up (increased Median Grad. Debt). 
+                  We believe this could have been a result of the $27k line we discovered which was unanimous among both residents and non-residents."),
+                 p("2. Our second conclusion found that people who have taken a loan out ever are more likely to have a larger median graduation loan. Although this seemed
+                   rather obvious this was an added hypothesis we tested given that the previous two graphs had unsuspected results. We are happy to see this relationship as it 
+                   gave us confidence in the results."),
+                 p(strong(em("Additional Finding - ")), "The median graduation debt was highest among hte lowest In State and Out of State Tuition graphs with additional
+                   points much lower in debt for more expensive colleges. This finding was unsuspected, as our intuition led us to believe that more expensive schools would yield 
+                   higher debts. However, with our visualization we easily discovered the oppositie. We believe this could be a product of more affluent people attending more expensive
+                   colleges which allows them to pay the tuition no matter the amount, therefoe skewing our hypothesis.")
                  ))),
 
                  
